566efcf8eb0a11ab611534c24c3d39dc1dacb023
BalazsGitHub <balazs.dombovari@gmail.com>
2022-09-05 14:28:53 +0200

    update2

--- src/test/java/com/example/jpastarti/JpaStartiApplicationTests.java
+++ src/test/java/com/example/jpastarti/JpaStartiApplicationTests.java
@@ -4,6 +4,7 @@ import com.example.jpastarti.model.Student;
 import com.example.jpastarti.repository.StudentRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.time.LocalDate;
@@ -12,7 +13,7 @@ import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-@SpringBootTest //it will start up the whole sprinboot project with all the beans
+@DataJpaTest //it will start up the whole sprinboot project with all the beans
 class JpaStartiApplicationTests {
 
     @Autowired
